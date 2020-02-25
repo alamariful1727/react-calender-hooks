2e4d545bc355055ca5ee30abd95d1d32fa3dbf00
Ariful Alam <alamariful1727@gmail.com>
2020-02-25 21:02:56 +0600

    postcss config updated

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,25 @@
 const tailwindcss = require("tailwindcss");
 
+const purgecss = require("@fullhuman/postcss-purgecss")({
+	// Specify the paths to all of the template files in your project
+	content: [
+		"./src/**/*.html",
+		"./src/**/*.tsx",
+		"./src/**/*.ts",
+		"./src/**/*.jsx",
+		"./src/**/*.js",
+		"./public/index.html"
+	],
+
+	// Include any special characters you're using in this regular expression
+	defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+	// defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
 module.exports = {
 	plugins: [
 		tailwindcss("./tailwind.js"),
 		require("autoprefixer"),
-		process.env.NODE_ENV === "production" &&
-			require("@fullhuman/postcss-purgecss")({
-				content: [
-					"./src/**/*.html",
-					"./src/**/*.tsx",
-					"./src/**/*.ts",
-					"./src/**/*.jsx",
-					"./src/**/*.js",
-					"./public/index.html"
-				],
-				defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-			})
+		process.env.NODE_ENV === "production" ? [purgecss] : []
 	]
 };
